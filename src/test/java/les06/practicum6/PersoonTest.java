679c05ffe50a09ec53fd82d0b400008f3ec093b0
Bart v. Eijkelenburg <bart.vaneijkelenburg@hu.nl>
2024-01-10 17:40:54 +0100

    Update Tests to 'nl' Locale

--- src/test/java/les06/practicum6/PersoonTest.java
+++ src/test/java/les06/practicum6/PersoonTest.java
@@ -1,14 +1,14 @@
 // package les06.practicum6;
-
+//
+// import org.junit.jupiter.api.BeforeAll;
 // import org.junit.jupiter.api.BeforeEach;
 // import org.junit.jupiter.api.Test;
-
-// import les6.Game;
-
+//
 // import java.time.LocalDate;
-
+// import java.util.Locale;
+//
 // import static org.junit.jupiter.api.Assertions.*;
-
+//
 // class PersoonTest {
 //     private int ditJaar;
 //     private Persoon koper;
@@ -27,7 +27,12 @@
 //     private String toStringTekstVerkoper0Games;
 //     private String toStringTekstVerkoperHeeftGame1;
 //     private String toStringTekstVerkoperHeeftGame2;
-
+//
+//     @BeforeAll
+//     public static void setup() {
+//         Locale.setDefault(Locale.of("nl"));
+//     }
+//
 //     @BeforeEach
 //     public void init(){
 //         ditJaar = LocalDate.now().getYear();
@@ -50,8 +55,8 @@
 //         toStringTekstVerkoperHeeftGame1 = "Jos de Verkoper heeft een budget van €10,00 en bezit de volgende games:";
 //         toStringTekstVerkoperHeeftGame2 = "Jos de Verkoper heeft een budget van €25,00 en bezit de volgende games:";
 //     }
-
-
+//
+//
 //     //region tests toString()
 //     @Test
 //     public void testToStringGeenGames(){
@@ -59,7 +64,7 @@
 //                 koper.toString(),
 //                 "toString() levert niet de juiste string op.");
 //     }
-
+//
 //     @Test
 //     public void testToString1Game(){
 //         koper.koop(game1);
@@ -68,7 +73,7 @@
 //                 koper.toString(),
 //                 "toString() levert niet de juiste string op.");
 //     }
-
+//
 //     @Test
 //     public void testToString2Games(){
 //         koper.koop(game1);
@@ -80,60 +85,60 @@
 //                 "toString() levert niet de juiste string op.");
 //     }
 //     //endregion
-
+//
 //     //region tests koop()
 //     @Test
 //     public void testResultKoopGameNieuwVoldoendeBudget(){
 //         boolean gelukt = koper.koop(game1);
 //         assertEquals(true, gelukt, "koop() geeft ten onrechte terug dat de koop niet gelukt is.");
 //     }
-
+//
 //     @Test
 //     public void testBudgetKoopGameNieuwVoldoendeBudget(){
 //         boolean gelukt = koper.koop(game1);
 //         assertEquals(150.0, Math.round(koper.getBudget() * 100)/100d, "koop() levert niet het goede budget op.");
 //     }
-
+//
 //     @Test
 //     public void testGameListKoopGameNieuwVoldoendeBudget(){
 //         boolean gelukt = koper.koop(game1);
 //         assertEquals(toStringTekstKoperHeeftGame1 + "\n" + toStringTekstGame1, koper.toString(),
 //                 "koop() moet gelukt zijn, maar levert een fout op in, waarschijnlijk, de lijst met Games.");
 //     }
-
+//
 //     @Test
 //     public void testResultKoopGameNieuwOnvoldoendeBudget(){
 //         boolean gelukt = koperArm.koop(game1);
 //         assertEquals(false, gelukt, "koop() geeft ten onrechte terug dat de koop gelukt is.");
 //     }
-
+//
 //     @Test
 //     public void testBudgetKoopGameNieuwOnvoldoendeBudget(){
 //         boolean gelukt = koperArm.koop(game1);
 //         assertEquals(36.0, Math.round(koperArm.getBudget() * 100)/100d, "koop() levert niet het goede budget op.");
 //     }
-
+//
 //     @Test
 //     public void testGameListKoopGameNieuwOnvoldoendeBudget(){
 //         boolean gelukt = koperArm.koop(game1);
 //         assertEquals(toStringTekstKoperArm, koperArm.toString(),
 //                 "koop() mag niet gelukt zijn, maar levert een fout op in, waarschijnlijk, de lijst met Games.");
 //     }
-
+//
 //     @Test
 //     public void testResultKoopDubbeleGameVoldoendeBudget(){
 //         boolean gelukt = koper.koop(game1);
 //         gelukt = koper.koop(game1);
 //         assertEquals(false, gelukt, "Game is al in bezit, maar koop() geeft ten onrechte terug dat de koop gelukt is.");
 //     }
-
+//
 //     @Test
 //     public void testBudgetKoopDubbeleGameVoldoendeBudget(){
 //         boolean gelukt = koper.koop(game1);
 //         gelukt = koper.koop(game1);
 //         assertEquals(150.0, Math.round(koper.getBudget() * 100)/100d, "koop() levert niet het goede budget op (game is al in bezit)");
 //     }
-
+//
 //     @Test
 //     public void testGameListKoopDubbeleGameVoldoendeBudget(){
 //         boolean gelukt = koper.koop(game1);
@@ -141,21 +146,21 @@
 //         assertEquals(toStringTekstKoperHeeftGame1 + "\n" + toStringTekstGame1, koper.toString(),
 //                 "koop() mag niet gelukt zijn (game is al in bezit), maar levert een fout op in, waarschijnlijk, de lijst met Games.");
 //     }
-
+//
 //     @Test
 //     public void testResultKoopDubbeleGameOnvoldoendeBudget(){
 //         boolean gelukt = koperArm.koop(game2);
 //         gelukt = koperArm.koop(game2);
 //         assertEquals(false, gelukt, "Game is al in bezit en er is te weinig budget, maar koop() geeft ten onrechte terug dat de koop gelukt is.");
 //     }
-
+//
 //     @Test
 //     public void testBudgetKoopDubbeleGameOnvoldoendeBudget(){
 //         boolean gelukt = koperArm.koop(game2);
 //         gelukt = koperArm.koop(game2);
 //         assertEquals(1.00, Math.round(koperArm.getBudget() * 100)/100d, "koop() levert niet het goede budget op (game is al in bezit).");
 //     }
-
+//
 //     @Test
 //     public void testGameListKoopDubbeleGameOnvoldoendeBudget(){
 //         boolean gelukt = koperArm.koop(game2);
@@ -164,21 +169,21 @@
 //                 "koop() mag niet gelukt zijn, maar levert een fout op in, waarschijnlijk, de lijst met Games.");
 //     }
 //     //endregion
-
+//
 //     //region tests verkoop()
 //     @Test
 //     public void testResultVerkoopGeenBezitVerkoper(){
 //         boolean gelukt = verkoper.verkoop(game1, koper);
 //         assertFalse(gelukt, "verkoop() geeft ten onrechte terug dat verkoop gelukt is.");
 //     }
-
+//
 //     @Test
 //     public void testBudgetVerkoopGeenBezitVerkoper(){
 //         boolean gelukt = verkoper.verkoop(game1, koper);
 //         assertEquals(60, Math.round(verkoper.getBudget() * 100)/100d,"verkoop() levert niet het goede budget van de verkoper op (Game is niet in bezit verkoper).");
 //         assertEquals(200, Math.round(koper.getBudget() * 100)/100d,"verkoop() levert niet het goede budget van de koper op (Game is niet in bezit verkoper).");
 //     }
-
+//
 //     @Test
 //     public void testGameListVerkoopGeenBezitVerkoper(){
 //         boolean gelukt = verkoper.verkoop(game1, koper);
@@ -187,14 +192,14 @@
 //         assertEquals(toStringTekstKoper0Games, koper.toString(),
 //                 "verkoop() mag niet gelukt zijn (Game is niet in bezit verkoper), maar levert een fout op in, waarschijnlijk, de lijst met Games (koper).");
 //     }
-
+//
 //     @Test
 //     public void testResultVerkoopKoperOnvoldoendeBudget(){
 //         verkoper.koop(game1);
 //         boolean gelukt = verkoper.verkoop(game1, koperArm);
 //         assertFalse(gelukt, "verkoop() geeft ten onrechte terug dat verkoop gelukt is.");
 //     }
-
+//
 //     @Test
 //     public void testBudgetVerkoopKoperOnvoldoendeBudget(){
 //         verkoper.koop(game1);
@@ -202,7 +207,7 @@
 //         assertEquals(10, Math.round(verkoper.getBudget() * 100)/100d,"verkoop() levert niet het goede budget van de verkoper op (game is niet verkocht).");
 //         assertEquals(36, Math.round(koperArm.getBudget() * 100)/100d,"verkoop() levert niet het goede budget van de koper op (game is niet verkocht).");
 //     }
-
+//
 //     @Test
 //     public void testGameListVerkoopKoperOnvoldoendeBudget(){
 //         verkoper.koop(game1);
@@ -212,7 +217,7 @@
 //         assertEquals(toStringTekstKoper0Games, koper.toString(),
 //                 "verkoop() mag niet gelukt zijn (koper onvoldoende budget), maar levert een fout op in, waarschijnlijk, de lijst met Games (koper).");
 //     }
-
+//
 //     @Test
 //     public void testResultVerkoopKoperDubbeleGame(){
 //         verkoper.koop(game2);
@@ -220,7 +225,7 @@
 //         boolean gelukt = verkoper.verkoop(game2, koper);
 //         assertFalse(gelukt, "verkoop() geeft ten onrechte terug dat verkoop gelukt is.");
 //     }
-
+//
 //     @Test
 //     public void testBudgetVerkoopKoperDubbeleGame(){
 //         verkoper.koop(game2);
@@ -228,7 +233,7 @@
 //         assertEquals(25, Math.round(verkoper.getBudget() * 100)/100d,"verkoop() levert niet het goede budget van de verkoper op (game is niet verkocht).");
 //         assertEquals(165, Math.round(koper.getBudget() * 100)/100d,"verkoop() levert niet het goede budget van de koper op (game is niet verkocht).");
 //     }
-
+//
 //     @Test
 //     public void testGameListVerkoopKoperDubbeleGame(){
 //         verkoper.koop(game2);
@@ -238,14 +243,14 @@
 //         assertEquals(toStringTekstKoperHeeftGame2 + "\n" + toStringTekstGame2, koper.toString(),
 //                 "verkoop() mag niet gelukt zijn (koper heeft game al), maar levert een fout op in, waarschijnlijk, de lijst met Games (koper).");
 //     }
-
+//
 //     @Test
 //     public void testResultVerkoopVoorwaardenOK(){
 //         verkoper.koop(game2);
 //         boolean gelukt = verkoper.verkoop(game2, koper);
 //         assertTrue(gelukt, "verkoop() geeft ten onrechte terug dat verkoop niet gelukt is.");
 //     }
-
+//
 //     @Test
 //     public void testBudgetVerkoopVoorwaardenOK(){
 //         verkoper.koop(game2);
@@ -253,7 +258,7 @@
 //         assertEquals(60, Math.round(verkoper.getBudget() * 100)/100d,"verkoop() levert niet het goede budget van de verkoper op (game is verkocht).");
 //         assertEquals(165, Math.round(koper.getBudget() * 100)/100d,"verkoop() levert niet het goede budget van de koper op (game is verkocht).");
 //     }
-
+//
 //     @Test
 //     public void testGameListVerkoopVoorwaardenOK(){
 //         verkoper.koop(game2);
@@ -263,5 +268,5 @@
 //         assertEquals(toStringTekstKoperHeeftGame2 + "\n" + toStringTekstGame2, koper.toString(),
 //                 "verkoop() moet gelukt zijn, maar levert een fout op in, waarschijnlijk, de lijst met Games (koper).");
 //     }
-//     //endregion 
+//     //endregion
 // }
